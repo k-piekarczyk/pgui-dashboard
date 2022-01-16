49cba42ae4e5e7bea81383cce4ee4a304f415cfa
mczarkowski7 <mczarkowski7@outlook.com>
2022-01-16 19:40:00 +0100

    wip

--- src/pages/SalesChart.page.tsx
+++ src/pages/SalesChart.page.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from "react-i18next";
 import PageContainer from "../components/PageContainer.component";
 import "./SalesChart.Page.scss";
+// import rd3 from 'react-d3-library';
 
 export default function SalesChart() {
   const { t } = useTranslation();
@@ -11,14 +12,42 @@ export default function SalesChart() {
       previousPage={{ route: "/dashboard", name: t("Dashboard") }}
       loggedIn={true}
     >
-      <div>
-        I cyk tu jest div
+      <div className="leftBar">
+        <div className="tile category">
+          <h4>Kategoria</h4>
+          <div>
+            <span className="radioIcon"/>
+            Obrót
+          </div>
+          <div>
+            <span className="radioIcon"/>
+            Liczba sprzedanych sztuk
+            <div>Wybierz produkt</div>
+          </div>
+        </div>
+        <div className="tile timePeriod">
+          <h4>Zakres czasu</h4>
+          <div>
+            <span className="radioIcon"/>
+            Dziś
+          </div>
+          <div>
+            <span className="radioIcon"/>
+            Obecny tydzień
+          </div>
+          <div>
+            <span className="radioIcon"/>
+            Obecny miesiąc
+          </div>
+          <div>
+            <span className="checkIcon"/>
+            Uwzględnij analogiczny okres poprzedzający
+          </div>
+        </div>
       </div>
-      <div>
-        drugi div jest tu
-      </div>
-      <div>
-        i trzeci divik jest tutaj!
+
+      <div className="tile graph">
+        To jest miejsce na wykres!!!
       </div>
     </PageContainer>
   )
