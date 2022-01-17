ae8d61810736ed8d873f73bc87555c535bfe1162
mczarkowski7 <mczarkowski7@outlook.com>
2022-01-17 02:39:59 +0100

    narodziny bar charta

--- src/pages/SalesChart.page.tsx
+++ src/pages/SalesChart.page.tsx
@@ -11,28 +11,149 @@ export default function SalesChart() {
   const [timePeriod, setTimePeriod] = useState("today");
   const [previousPeriod, setPreviousPeriod] = useState(false);
 
+  function buildGraph(data: ({ time: string; value: number })[]) {
+    console.log("siemanko!");
+
+    // define margin
+    const margin = {
+      top: 40,
+      right: 40,
+      bottom: 40,
+      left: 40
+    }
+
+    //define size
+    const width = 600;
+    const height = 400;
+
+    // define y scale
+    const yMax = Math.max(...data.map((d) => d.value));
+    const y = d3
+      .scaleLinear()
+      .domain([0, yMax])
+      .range([height - margin.bottom, margin.top])
+    ;
+
+    // define x scale
+    const x = d3
+      .scaleBand()
+      .domain(data.map(d => d.time))
+      .range([margin.left, width - margin.right])
+      .padding(0.3);
+
+
+    // define svg
+    d3.select("svg").remove();
+    const svg = d3
+      .select('.graph')
+      .append("svg")
+      .attr("height", height)
+      .attr("width", width);
+
+    // define axis
+    // @ts-ignore
+    const yAxis = (g) => g.attr('transform', `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y));
+
+    // @ts-ignore
+    const xAxis = (g) =>
+      g.attr('transform', `translate(0, ${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+    // define bars
+    // @ts-ignore
+    const bars = (g) => g.selectAll('rect').data(data).join(
+      // @ts-ignore
+      (enter) => enter.append('rect'),
+      // @ts-ignore
+      (update) => update,
+      // @ts-ignore
+      (exit) => exit.remove()
+    )
+      .attr('x', (d: { time: string; }) => x(d.time))
+      .attr('y', (d: { value: d3.NumberValue; }) => y(d.value))
+      .attr('height', (d: { value: d3.NumberValue; }) => y(0) - y(d.value))
+      .attr('width', 20);
+
+    // put everything together
+    svg.append('g').call(yAxis);
+    svg.append('g').call(xAxis);
+    svg.append('g').call(bars);
+
+  }
+
+  useEffect(() => {
+    buildGraph(turnoverWeek);
+  })
+
+  const turnoverWeek = [
+    { time: "Poniedziałek", value: 1000 },
+    { time: "Wtorek", value: 1236 },
+    { time: "Środa", value: 1420 },
+    { time: "Czwartek", value: 1089 },
+    { time: "Piątek", value: 1690 },
+    { time: "Sobota", value: 2137 },
+
+  ];
+
+/*
+  const data = [1, 2, 3, 4, 5, 8];
+
+
+  const yMax = Math.max(...turnoverWeek.map(d => d.value));
+  const width = 600;
+  const height = 400;
+  const y = d3.scaleLinear()
+      .domain([0, yMax])
+      .range([0, height])
+
+  const x = d3.scaleBand()
+    .domain(turnoverWeek.map(d => d.time))
+    .range([0, width])
+    .padding(0.1)
+
+
   useEffect(() => {
     d3.select("svg").remove();
 
     const svg = d3
       .select('.graph')
       .append("svg")
-      .attr("height", 400)
-      .attr("width", 600);
+      .attr("height", height)
+      .attr("width", width);
 
-    const data = [1, 2, 3, 4, 5, 8];
+  console.log(x("Wtorek"))
 
+    // wartości na wykresie (prostokąty)
     svg.selectAll('rect')
-      .data(data)
+      .data(turnoverWeek)
       .enter()
       .append('rect')
       .attr('width', 20)
       .attr('height', 20)
       .attr('fill', 'orange')
-      .attr('x', d => d*30)
-      .attr('y', d => d*30)
-  });
+      .attr('x', d => x(d.time) ?? null)
+      .attr('y', d => height - y(d.value) ?? null);
 
+  // oś X
+    const xAxisScale = d3.scaleBand()
+      .domain(turnoverWeek.map(d => d.time))
+      .range([0, width])
+      .padding(0.1)
+
+    const xAxis = d3.axisBottom(xAxisScale)
+      .tickFormat((d, i) => d)
+      .tickSizeOuter(0)
+
+    const xAxisTranslate = height-20;
+
+    svg.append('g')
+      .attr("transform", "translate(0, " + xAxisTranslate + ")")
+      .call(xAxis)
+
+
+  });
+*/
 
 
   return (
